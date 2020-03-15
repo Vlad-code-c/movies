fca0aa3ee6bf4b0c76f74ac9d4e676d0347a9be1
Vlad-code-c <vladz1992b@gmail.com>
2020-03-15 12:20:50 +0200

    Updates

---
+++ precache-manifest.5450af0dee38211844a2d498a854b0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae27c62086b521c4039dadf8a8b62c6",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "c99e723f32e3ca358047",
+    "url": "/movies/static/css/main.aaa7fc17.chunk.css"
+  },
+  {
+    "revision": "44dc44962d9b03ebb48d",
+    "url": "/movies/static/js/2.93759603.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movies/static/js/2.93759603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99e723f32e3ca358047",
+    "url": "/movies/static/js/main.a8a8ccf8.chunk.js"
+  },
+  {
+    "revision": "a1257651323618fd983f",
+    "url": "/movies/static/js/runtime-main.c266acfd.js"
+  }
+]);
